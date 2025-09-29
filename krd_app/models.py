2af1d64407c29d073bbf8d9d252956c8e45a4df7
Trippxzz <cris.ct2003@gmail.com>
2025-09-28 23:29:34 -0300

    form prod

--- krd_app/models.py
+++ krd_app/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Producto(models.Model):
+    id_producto = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    n_producto=models.CharField(max_length=60)
+    desc_producto=models.CharField(max_length=200)
+    precio=models.IntegerField()
+    imagen=models.ImageField(upload_to="prodimagen") ##Se guardara de forma local hasta cambiar modo de almacenamiento en hosting
+    aro = models.IntegerField(max_length=2)
+    apernadura=models.CharField(max_length=7)
+    ancho=models.CharField(max_length=5)
+    offset=models.IntegerField(max_length=3)
+    centro_llanta=models.CharField(max_length=5)
+
--- krd_app/views.py
+++ krd_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django import forms 
+from django.contrib.auth.forms import UserCreationForm ##Para la creacion de usuarios, se usará para completar datos al momento de comprar
 # Create your views here.
+
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = ModelProd
+        exclude = ("idproducto", "idvehiculo", "stock")
